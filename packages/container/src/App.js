f36bb3c1c54a3e83dddf71ce144f08061995e599
todorovicstefan989 <todorovicstefan989@gmail.com>
2023-02-06 12:41:09 +0100

    Generate default classname prefix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
 import MarketingApp from "./components/MarketingApp";
 import Header from './components/Header';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'core'
+});
+
 export default () => {
     return (
-        <BrowserRouter>
-            <div>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
                 <Header />
                 <MarketingApp />
-            </div>
-        </BrowserRouter>
+            </BrowserRouter>
+        </StylesProvider>
     );
 };
